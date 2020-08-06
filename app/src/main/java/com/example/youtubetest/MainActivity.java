07a3b264184161ed6ddc4d411620ed5fc66d9cde
Md Shahrar Fatemi <shahrarswapnil@gmail.com>
2020-08-06 15:13:10 +0600

    first commit

---
+++ app/src/main/java/com/example/youtubetest/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.youtubetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity {
+    YouTubePlayerView youTubePlayerView;
+    Button button;
+    YouTubePlayer.OnInitializedListener onInitializedListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.buttonID);
+        youTubePlayerView = (YouTubePlayerView)findViewById(R.id.youtubePlayer);
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("3LiubyYpEUk");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                youTubePlayerView.initialize(PlayerConfig.API_key,onInitializedListener);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/youtubetest/PlayerConfig.java
@@ -0,0 +1,8 @@
+package com.example.youtubetest;
+
+public class PlayerConfig {
+    public static String API_key = "AIzaSyCPbwi0qG2zciOU83gLlM9ydUSGfy98ee8";
+    PlayerConfig(){
+
+    }
+}
